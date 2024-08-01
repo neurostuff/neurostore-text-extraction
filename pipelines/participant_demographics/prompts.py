f2cf69a6154ae1eb8182c4730ffe46c5cbcb2723
Alejandro de la Vega <aleph4@gmail.com>
2024-08-01 17:33:03 -0500

    Add instruction to extract imaging sampel

--- pipelines/participant_demographics/prompts.py
+++ pipelines/participant_demographics/prompts.py
@@ -40,6 +40,7 @@ For each group identify:
     - the number of participants in each group, and the diagnosis.
     - the number of male participants, and their mean age, median age, minimum and maximum age
     - the number of female participants, and their mean age, median age, minimum and maximum age.
+    - if this group of participants underwent MRI, fMRI or neuroimaging procedure.
 
 Be as accurate as possible, and report information (especially diagnosis) using the technical terms (and abbreviations) used in the article.
 If any of the information is missing, return `null` for that field.
