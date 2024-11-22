8c5237f98de3568be39aef7b12d515b9d3cf2e32
James Kent <jamesdkent21@gmail.com>
2024-11-21 18:27:05 -0600

    work with .keys file

--- tests/test_participant_demographics.py
+++ tests/test_participant_demographics.py
@@ -9,10 +9,10 @@ from ns_pipelines.dataset import Dataset
 def test_ParticipantDemographicsExtractor(sample_data, tmp_path):
     """Test the word count extraction pipeline."""
     pde = ParticipantDemographicsExtractor(
-        extraction_model="gpt-4o-2024-08-06",
+        extraction_model="gpt-4o-mini-2024-07-18",
         prompt_set="ZERO_SHOT_MULTI_GROUP_FTSTRICT_FC",
         env_variable="API_CLIENT_OPENAI_KEY",
-        env_file=str(Path(__file__).parents[1] / ".env"),
+        env_file=str(Path(__file__).parents[1] / ".keys"),
     )
     dataset = Dataset(sample_data)
     output_dir = tmp_path / "participant_demographics"
